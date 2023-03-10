962f8a84e06adfb038dc3c15c61f0809c72fe62e
SimoneLollini <lollinisimone@gmail.com>
2023-03-10 15:54:01 +0100

    add setter and getter ex 1

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,13 +1,55 @@
 package org.lessons.java.shop;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Product[] products = new Product[3]; // creo un array in cui inseriró 5 prodotti
 
-        Product[] products = new Product[5];
+        System.out.println(Arrays.toString(products));
 
-        System.out.println(Arrays.toString(products)); // creo un array in cui inseriro 5 prodotti
+        System.out.println("Scrivi le informazioni di questi 3 prodotti!");
+        for (int i = 0; i < products.length; i++) {
+            products[i] = new Product();
+            System.out.println(products[i].getCode());
+
+            System.out.print("Nome prodotto: ");
+            String name = scan.next(); //chiedo il nome del prodotto
+            products[i].setName(name); //set product's name
+
+            System.out.print("Descrizione prodotto: ");
+            String description = scan.next();
+            products[i].setDescription(description); //set product's description
+
+            System.out.print("price prodotto: ");
+            double price = scan.nextDouble();
+            products[i].setPrice(price); //set product's price
+
+            System.out.print("Iva prodotto: ");
+            double iva = scan.nextDouble();
+            products[i].setIva(iva); //set product's iva
+
+
+            System.out.print("Nome: ");
+            System.out.println(products[i].getName());
+
+            System.out.print("Descrizione: ");
+            System.out.println(products[i].getDescription());
+
+            System.out.print("Prezzo: ");
+            System.out.println(products[i].getPrice());
+
+            System.out.print("Prezzo compreso iva: ");
+            System.out.println(products[i].getIvaPrice());
+
+            System.out.print("Iva: ");
+            System.out.println(products[i].getIva());
+
+        }
+
+
 
     }
 }
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -2,22 +2,11 @@ package org.lessons.java.shop;
 
 import java.util.Random;
 
-/*  Un prodotto è caratterizzato da:
-- codice (numero intero)
-- nome
-- descrizione
-- prezzo
-- iva
-Usate opportunamente i livelli di accesso (public, private),
- i costruttori, i metodi getter e setter ed eventuali altri metodi di “servizio” per fare in modo che:
-alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
-Il codice prodotto sia accessibile solo in lettura
-Gli altri attributi siano accessibili sia in lettura che in scrittura
+/*
 Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
 Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
-Nello stesso package aggiungete una classe Main con metodo main nella quale testate tutte le funzionalità della classe Prodotto
 BONUS: create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri
-(ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è) Buon lavoro!  */
+(ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)  */
 public class Product {
     Random rand = new Random();
     private int code;
@@ -27,7 +16,7 @@ public class Product {
     private double iva;
 
     public Product() {
-        code = rand.nextInt();
+        code = rand.nextInt( 100000);
         name = "";
         description ="";
         price = 0;
@@ -41,4 +30,55 @@ public class Product {
         this.price = price;
         this.iva = iva;
     }
+
+    //get product's code
+    public int getCode() {
+        return code;
+    }
+
+    //get and set name
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    //get and set product's description
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    //get and set product's price
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+
+    //get and set product's iva
+    public double getIva(){
+        return iva;
+    }
+
+    public void setIva(double iva){
+        this.iva = iva;
+    }
+
+
+
+    public double getIvaPrice(){
+        return price * iva;
+    }
+
+
 }
